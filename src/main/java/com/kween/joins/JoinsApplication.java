38a29c492bc7543084151dd074b3d7fe3c907737
deepseekai <deepseekai.usa@gmail.com>
2026-05-10 15:01:16 -0400

    Initial version

---
+++ src/main/java/com/kween/joins/JoinsApplication.java
@@ -0,0 +1,18 @@
+package com.kween.joins;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoinsApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(JoinsApplication.class);
+
+	public static void main(String[] args) {
+
+        LOGGER.info("JoinsApplication start");
+        SpringApplication.run(JoinsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/kween/joins/JoinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kween.joins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

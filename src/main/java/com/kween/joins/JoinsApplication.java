b3fab335a0b741f7b7fb2cac2aa4080d3f7a5ff4
deepseekai <deepseekai.usa@gmail.com>
2026-05-12 05:02:00 -0400

    add a sout line to feature branch

--- src/main/java/com/kween/joins/JoinsApplication.java
+++ src/main/java/com/kween/joins/JoinsApplication.java
@@ -14,6 +14,7 @@ public class JoinsApplication {
 
         LOGGER.info("JoinsApplication start");
         SpringApplication.run(JoinsApplication.class, args);
+        System.out.println("JoinsApplication start");
         LOGGER.info("JoinsApplication end");
         System.exit(0);
     }
